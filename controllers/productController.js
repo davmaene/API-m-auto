f24c96a0dfbd4563c82f1611de605e75c94209c9
davmaene <davidmened@gmail.com>
2021-10-01 14:24:52 +0200

    up lin

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,7 +9,19 @@ import database from '../config/database';
 dotenv.config();
 
 const productController = {
-    listerProduits : async (req, res) => {
+    // runrow
+    listerProduits: async () => {
+        
+       await database.query("ALTER TABLE `products` ADD `imgcover` VARCHAR(60) NOT NULL AFTER `nom`")
+        .then(d => {
+            console.log(d);
+// CD Player	Sun Roof	Leather Seat	Alloy Wheels Power Steering	Power Window	A/C	ABS Airbag	Radio	CD Changer	DVD TV	Power Seat	Back Tire	Grill GuardRear Spoiler	Central Locking	Jack	Spare Tire Wheel Spanner	Fog Lights	Back Camera	Push Start Keyless Entry	ESC	360 Degree Camera	Body Kit   Side Airbag	Power Mirror	Side Skirts	Front Lip Spoiler Navigation Turbo
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    },
+    listerProduitss : async (req, res) => {
          let results = await produits.findAll({
             // where: {
             //     status: 1
